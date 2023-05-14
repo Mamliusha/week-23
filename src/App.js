36ba8590866e018c93475d30b7dd5f6dbe1f394e
Mamliusha <109955386+Mamliusha@users.noreply.github.com>
2023-05-14 21:51:17 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './tarif.css'
 
-function App() {
+function Tariff() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='tariffs'>
+    <div className='tar' id='tar1'>
+    <div className='tar1'>Безлимитный 300</div>
+    <div className='tar2'>300 руб./мес.</div>
+    <div className='tar3'>До 10 Мбит/с.
+    Объем включенного трафика неограничен.</div>  
     </div>
+    <div className='tar' id='tar2'>
+    <div className='tar1'>Безлимитный 450</div>
+    <div className='tar2'>450 руб./мес.</div>
+    <div className='tar3'>До 50 Мбит/с.
+    Объем включенного трафика неограничен.</div>  
+    </div>
+    <div className='tar' id='tar3'>
+    <div className='tar1'>Безлимитный 550</div>
+    <div className='tar2'>550 руб./мес.</div>
+    <div className='tar3'>До 100 Мбит/с.
+    Объем включенного трафика неограничен.</div>  
+    </div>
+    <div className='tar' id='tar4'>    
+    <div className='tar1'>Безлимитный 1000</div>
+    <div className='tar2'>1000 руб./мес.</div>
+    <div className='tar3'>До 200 Мбит/с.
+    Объем включенного трафика неограничен.</div>  
+    </div>
+  </div>
   );
 }
 
-export default App;
+export default Tariff;
